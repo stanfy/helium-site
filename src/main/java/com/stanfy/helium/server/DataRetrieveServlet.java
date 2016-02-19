729e7e584a167a4896be41fee26c1d541c002b88
Olexandr Shapovalov <shapovalovos@gmail.com>
2016-02-19 10:07:01 -0800

    Respond 400 error on wrong api requests

--- src/main/java/com/stanfy/helium/server/DataRetrieveServlet.java
+++ src/main/java/com/stanfy/helium/server/DataRetrieveServlet.java
@@ -13,50 +13,64 @@ import java.util.HashMap;
 
 public class DataRetrieveServlet extends HttpServlet {
 
-  public static final int INITIAL_CAPACITY = 500;
+  private static final int INITIAL_CAPACITY = 500;
+  private static final String JSON_CONTENT_TYPE = "application/json";
+  private static final String PATH_DATA = "/data/";
   private static final HashMap<String, Data> STORE = new HashMap<>(INITIAL_CAPACITY);
   private static final Gson GSON = new GsonBuilder().serializeNulls().create();
-  private static final String JSON_CONTENT_TYPE = "application/json";
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    String dataId = getDataId(req);
-
-    Data data = STORE.get(dataId);
-    String jsonData = buildResponse(data);
-
     setContentType(resp);
-    putClientResponse(resp, jsonData);
+
+    String dataId = getDataId(req);
+    if (dataId != null) {
+      Data data = STORE.get(dataId);
+      String jsonData = buildResponse(data);
+      putClientResponse(resp, jsonData);
+    } else {
+      setErrorStatusCode(resp);
+    }
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    Data requestData = GSON.fromJson(req.getReader(), Data.class);
-    String dataId = getDataId(req);
-
-    STORE.put(dataId, requestData);
-    String jsonData = GSON.toJson(requestData);
-
     setContentType(resp);
-    putClientResponse(resp, jsonData);
+
+    String dataId = getDataId(req);
+    if (dataId != null) {
+      Data requestData = GSON.fromJson(req.getReader(), Data.class);
+      STORE.put(dataId, requestData);
+      String jsonData = GSON.toJson(requestData);
+      putClientResponse(resp, jsonData);
+    } else {
+      setErrorStatusCode(resp);
+    }
   }
 
   @Override
   protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    String dataId = getDataId(req);
-    STORE.remove(dataId);
-
     setContentType(resp);
+
+    String dataId = getDataId(req);
+    if (dataId != null) {
+      STORE.remove(dataId);
+    } else {
+      setErrorStatusCode(resp);
+    }
   }
 
   @Override
   protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    Data requestData = GSON.fromJson(req.getReader(), Data.class);
+    setContentType(resp);
 
     String dataId = getDataId(req);
-
-    STORE.put(dataId, requestData);
-    setContentType(resp);
+    if (dataId != null) {
+      Data requestData = GSON.fromJson(req.getReader(), Data.class);
+      STORE.put(dataId, requestData);
+    } else {
+      setErrorStatusCode(resp);
+    }
   }
 
   private String buildResponse(final Data data) {
@@ -72,10 +86,28 @@ public class DataRetrieveServlet extends HttpServlet {
   }
 
   private String getDataId(HttpServletRequest req) {
-    return req.getPathInfo().replaceFirst("/", "");
+    String path = req.getRequestURI();
+
+    if (path == null || path.isEmpty()) {
+      return null;
+    }
+
+    String dataId = null;
+    if (path.startsWith(PATH_DATA)) {
+      dataId = path.replaceFirst(PATH_DATA, "");
+      if (dataId.isEmpty()) {
+        return null;
+      }
+    }
+
+    return dataId;
   }
 
   private void setContentType(HttpServletResponse resp) {
     resp.setContentType(JSON_CONTENT_TYPE);
+  }
+
+  private void setErrorStatusCode(HttpServletResponse resp) {
+    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
   }
 }
